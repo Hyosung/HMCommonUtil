73679993a38a20780d520d89650f55be8c5f7839
Hyosung <sy92710xx@163.com>
2013-12-11 13:48:23 +0800

    Initial Commit

---
+++ HMCommUtil/HMCommUtil.h
@@ -0,0 +1,13 @@
+//
+//  HMCommUtil.h
+//  HMCommUtil
+//
+//  Created by ismallstar on 13-12-11.
+//  Copyright (c) 2013年 iyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMCommUtil : NSObject
+
+@end
---
+++ HMCommUtil/HMCommUtil.m
@@ -0,0 +1,13 @@
+//
+//  HMCommUtil.m
+//  HMCommUtil
+//
+//  Created by ismallstar on 13-12-11.
+//  Copyright (c) 2013年 iyun. All rights reserved.
+//
+
+#import "HMCommUtil.h"
+
+@implementation HMCommUtil
+
+@end
